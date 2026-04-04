ae2470b7925b53552d386e6a25a6917bd4813de1
Karan Sehrawat <azcreations2688@gmail.com>
2026-04-04 17:41:43 +0530

    4TH DAY

--- Backend/src/lib/auth.js
+++ Backend/src/lib/auth.js
@@ -1,9 +1,14 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./db.js";
+import { expo } from "@better-auth/expo";
 
 export const auth = betterAuth({
+    plugins: [expo()],
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
+    emailAndPassword: {
+        enabled: true
+    }
 });
